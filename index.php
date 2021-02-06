20b88884fdafe7231f15794bd8fd3be5983a212f
Alex Underwood <underwoodapu@gmail.com>
2021-02-05 16:13:09 -0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,49 @@
+<?php
+    //$myVar1 = 'Hello World!';
+    //echo $myVar1;
+
+    //$numbers = array(12, 4, 5, 17, 8);
+    //print_r($numbers);
+    //echo $numbers[1];
+    /*
+    $ages = array(
+        "John" => 35,
+        "Mary" => 27,
+        "Bob" => 55
+    );
+    */
+    //echo $ages['Mary'];
+    //echo count($ages);
+    /*
+    for($i = 0; $i <= 5; $i++){
+        echo $i;
+    }
+    */
+    /*
+    $i = 0;
+    while($i <= 10){
+        echo 'Number ' .$i. '<br />';
+        $i++;
+    }
+
+    $numbers = array(12, 4, 5, 17, 8);
+    foreach($numbers as $number){
+        echo 'This is number ' .$number. '<br />';
+    }
+    */
+
+    //Functions
+    /*
+    function greet($name){
+        echo 'Hello '. $name;
+    }
+
+    greet('Alex'); */
+    // Operators
+    $num = 10;
+
+    if($num == 10){
+        echo 'Correct!';
+    }
+
+?>
